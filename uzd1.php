23788dbffd3a3c0f4411ccfcd436dc10d66aec01
naglismoc <dvd.filmas@gmail.com>
2023-12-12 09:31:59 +0200

    code cleanup

--- uzd1.php
+++ uzd1.php
@@ -1,23 +1,24 @@
 <?php
 $color = "black";
-if(isset($_GET["color"]) && $_GET["color"] == 1){
+if (isset($_GET["color"]) && $_GET["color"] == 1) {
     $color = "red";
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background-color: <?=$color?>;">
+
+<body style="background-color: <?= $color ?>;">
     <a href="./uzd1.php">be be be</a>
     <a href="./uzd1.php?color=1">su</a>
-    
 </body>
+
 </html>
 
 <!-- Sukurti puslapį su juodu fonu ir su dviem linkais (nuorodom) į save. Viena nuoroda su failo vardu, o kita nuoroda su failo vardu ir GET duomenų  perdavimo metodu perduodamu kintamuoju color=1. Padaryti taip, kad paspaudus ant nuorodos su perduodamu kintamuoju fonas nusidažytų raudonai, o paspaudus ant nuorodos be perduodamo kintamojo, vėl pasidarytų juodas. -->
